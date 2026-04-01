22c0d927ad33b7787f037c822f92e71a08262a49
Eldanaf <mo.eldanff@gmail.com>
2026-04-01 11:22:19 +0200

    Initial Commit

---
+++ OpenFiber-Technician/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OpenFiber-Technician
+//
+//  Created by IFTS 24 on 01/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OpenFiber-Technician/OpenFiber_TechnicianApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenFiber_TechnicianApp.swift
+//  OpenFiber-Technician
+//
+//  Created by IFTS 24 on 01/04/26.
+//
+
+import SwiftUI
+
+@main
+struct OpenFiber_TechnicianApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
